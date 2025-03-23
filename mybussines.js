99487a4bbe29ad79356e1fc8770914c1e4e55e5f
petrudemian <petredemi@gmail.com>
2025-03-23 16:55:57 +0000

    finel stage

--- mybussines.js
+++ mybussines.js
@@ -5,18 +5,32 @@ const contactbtn = document.querySelector('div.contact')
 
 const content = document.querySelector('.content')
 const contactpage = document.querySelector('div.contactpage')
+const servicespage = document.querySelector('div.servicespage') 
 
 homebtn.addEventListener('click', (e) =>{
     content.setAttribute('style', 'display:flex')
-    homebtn.setAttribute('style', 'background-color: green')
-    contactbtn.setAttribute('style', 'background-color: none')
+    servicespage.setAttribute('style', 'display: none')
     contactpage.setAttribute('style', 'display: none')
+    homebtn.setAttribute('style', 'background-color: lightgreen')
+    servbtn.setAttribute('style', 'background-color: none')
+    contactbtn.setAttribute('style', 'background-color: none')
 })
+servbtn.addEventListener('click', (e) => {
+    content.setAttribute('style', 'display: none')
+    servicespage.setAttribute('style', 'display: block')
+    contactpage.setAttribute('style', 'display: none')
+    homebtn.setAttribute('style', 'background-color: none')
+    servbtn.setAttribute('style', 'background-color: lightgreen')
+    contactbtn.setAttribute('style', 'background-color: none')
+})
+
+
+
 contactbtn.addEventListener('click', (e) => {
     content.setAttribute('style', 'display: none')
+    servicespage.setAttribute('style', 'display: none')
     contactpage.setAttribute('style', 'display: block')
     homebtn.setAttribute('style', 'background-color: none')
-    contactbtn.setAttribute('style', 'background-color: green')
-
-
+    servbtn.setAttribute('style', 'background-color: none')
+    contactbtn.setAttribute('style', 'background-color: lightgreen')
 })
